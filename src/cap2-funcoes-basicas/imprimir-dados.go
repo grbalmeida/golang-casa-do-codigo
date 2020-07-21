0bca0f527266fcdb8a724d4c6e68031b2cd63fab
gilvan <g.r.almeida@live.com>
2020-07-20 21:51:16 -0300

    Quicksort

--- src/cap2-funcoes-basicas/imprimir-dados.go
+++ src/cap2-funcoes-basicas/imprimir-dados.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func imprimirDados(nome string, idade int) {
-	fmt.Printf("%s tem %d anos.", nome, idade)
+	fmt.Printf("%s tem %d anos.\n", nome, idade)
 }
 
 func main() {
 	imprimirDados("Fernando", 29)
+	fmt.Println(soma(3, 5))
 }
